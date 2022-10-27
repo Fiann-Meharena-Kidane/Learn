097f0c16ebbbf76c976ca342d36e6a3d4ed9db4a
finan <finanmeharenakidane@gmail.com>
2022-10-27 15:36:30 +0400

    added event listener on translate button, added API

--- LangTranslationApp/index.js
+++ LangTranslationApp/index.js
@@ -98,13 +98,37 @@ const countries = {
     "zu-ZA": "Zulu"
 }
 
+
+const translateButton=document.querySelector('button');
+const fromText=document.querySelector('.from-text')
+const toText=document.querySelector('.to-text');
 const selectTag=document.querySelectorAll('select');
 
 
-selectTag.forEach(tag=> {
+
+selectTag.forEach((tag, id)=> {
     for(const country_code in countries) {
-        let option=`<option value="${country_code}">${countries[country_code]}</option>`
+        let selected;
+
+        if (id==0 && country_code=='en-GB'){
+            selected='selected';
+
+        }else if(id==1 && country_code=='hi-IN'){
+            selected='selected';
+        }
+        let option=`<option value="${country_code}" ${selected}>${countries[country_code]}</option>`
 
         tag.insertAdjacentHTML('beforeend', option);
     }
+})
+
+translateButton.addEventListener('click', ()=>{
+    let text=fromText.value;
+    translateFrom=selectTag[0].value;
+    translateTo=selectTag[1].value;
+    let apiURL=`https://api.mymemory.translated.net/get?q=${text}&langpair=${translateFrom}|${translateTo}`
+
+    fetch(apiURL).then(res  => res.json()).then(data => {
+        toText.value=data.responseData.translatedText;
+    })
 })
